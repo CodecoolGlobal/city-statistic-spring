73363dc26369dfd48053c24900c95cccdd9b14df
aron <pachl.aron@gmail.com>
2020-07-27 17:07:04 +0200

    init

---
+++ citystatistics/src/main/java/com/codecool/citystatistics/CitystatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.citystatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitystatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitystatisticsApplication.class, args);
+    }
+
+}

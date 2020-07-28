0baad8d2d24fdfafadcb542fc4a5beee50804574
aron <pachl.aron@gmail.com>
2020-07-28 16:35:21 +0200

    3 default city

--- citystatistics/src/main/java/com/codecool/citystatistics/controller/Controller.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/controller/Controller.java
@@ -8,11 +8,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @RestController
@@ -25,18 +28,25 @@ public class Controller {
     CityScoresCreator cityScores;
 
     @GetMapping("/")
-    public CityScores result() throws IOException, JSONException {
-        ArrayList<Score> scoreArrayList = new ArrayList<>();
-        CityScores cityResult = cityScores.createCityScores();
-        JSONObject result = apiCall.getResult("https://api.teleport.org/api/urban_areas/slug:budapest/scores/");
+    @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
+    public ArrayList<CityScores> result() throws IOException, JSONException {
+        ArrayList<CityScores> listOfCitiesByScores = new ArrayList<>();
+        List<String> defaultCities = Arrays.asList("budapest", "barcelona", "melbourne");
+        for (String defaultCity : defaultCities){
+            ArrayList<Score> scoreArrayList = new ArrayList<>();
+            CityScores cityResult = cityScores.createCityScores();
+            JSONObject result = apiCall.getResult("https://api.teleport.org/api/urban_areas/slug:"+ defaultCity +"/scores/");
 
-        JSONArray categories = (JSONArray) result.get("categories");
-        for (int i = 0; i < categories.length(); i++) {
-            scoreArrayList.add(new Score(categories.getJSONObject(i).getString("name"),categories.getJSONObject(i).getInt("score_out_of_10")));
+            JSONArray categories = (JSONArray) result.get("categories");
+            for (int i = 0; i < categories.length(); i++) {
+                scoreArrayList.add(new Score(categories.getJSONObject(i).getString("name"),categories.getJSONObject(i).getInt("score_out_of_10")));
+            }
+
+            cityResult.setCityName(defaultCity);
+            cityResult.setScores(scoreArrayList);
+            listOfCitiesByScores.add(cityResult);
         }
 
-        cityResult.setCityName("Budapest");
-        cityResult.setScores(scoreArrayList);
-        return cityResult;
+        return listOfCitiesByScores;
     }
 }

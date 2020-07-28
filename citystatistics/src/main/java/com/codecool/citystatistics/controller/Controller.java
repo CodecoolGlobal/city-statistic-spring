7daf7be40650b867c877b23ae6e9cd6d77ed40c9
TiborGulyas <gulyastibor@gmail.com>
2020-07-28 23:46:57 +0200

    default query - API completed

--- citystatistics/src/main/java/com/codecool/citystatistics/controller/Controller.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/controller/Controller.java
@@ -1,8 +1,10 @@
 package com.codecool.citystatistics.controller;
 
 import com.codecool.citystatistics.instance_creator.CityScoresCreator;
+import com.codecool.citystatistics.instance_creator.CitySmallCardCreator;
 import com.codecool.citystatistics.model.ApiCall;
 import com.codecool.citystatistics.model.CityScores;
+import com.codecool.citystatistics.model.CitySmallCard;
 import com.codecool.citystatistics.model.Score;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,19 +29,28 @@ public class Controller {
     @Autowired
     CityScoresCreator cityScores;
 
+    @Autowired
+    CitySmallCardCreator citySmallCard;
+
 
     @GetMapping("/")
     @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
-    public void defaultData(){
+    public ArrayList<CitySmallCard> defaultData() throws IOException, JSONException {
         List<String> defaultCities = Arrays.asList("budapest", "barcelona", "melbourne");
-        String searchCityURL = "https://api.teleport.org/api/cities/?search=";
+        ArrayList<CitySmallCard> citySmallCards = new ArrayList<>();
+        String searchCityURL = "https://api.teleport.org/api/urban_areas/slug:";
         for (String defaultCity : defaultCities){
-
+            CitySmallCard cityCard = citySmallCard.createCitySmallCard();
+            JSONObject resultName = apiCall.getResult(searchCityURL + defaultCity+"/");
+            String fullName = resultName.getString("full_name");
+            cityCard.setCityName(fullName);
+            JSONObject resultImage = apiCall.getResult(searchCityURL + defaultCity+"/images");
+            String imageURL = resultImage.getJSONArray("photos").getJSONObject(0).getJSONObject("image").getString("web");
+            cityCard.setImage(imageURL);
+            citySmallCards.add(cityCard);
         }
+        return citySmallCards;
     }
-
-
-
 
 
     @GetMapping("/search")
@@ -47,14 +58,14 @@ public class Controller {
     public ArrayList<CityScores> result() throws IOException, JSONException {
         ArrayList<CityScores> listOfCitiesByScores = new ArrayList<>();
         List<String> defaultCities = Arrays.asList("budapest", "barcelona", "melbourne");
-        for (String defaultCity : defaultCities){
+        for (String defaultCity : defaultCities) {
             ArrayList<Score> scoreArrayList = new ArrayList<>();
             CityScores cityResult = cityScores.createCityScores();
-            JSONObject result = apiCall.getResult("https://api.teleport.org/api/urban_areas/slug:"+ defaultCity +"/scores/");
+            JSONObject result = apiCall.getResult("https://api.teleport.org/api/urban_areas/slug:" + defaultCity + "/scores/");
 
             JSONArray categories = (JSONArray) result.get("categories");
             for (int i = 0; i < categories.length(); i++) {
-                scoreArrayList.add(new Score(categories.getJSONObject(i).getString("name"),categories.getJSONObject(i).getInt("score_out_of_10")));
+                scoreArrayList.add(new Score(categories.getJSONObject(i).getString("name"), categories.getJSONObject(i).getInt("score_out_of_10")));
             }
 
             cityResult.setCityName(defaultCity);
--- citystatistics/src/main/java/com/codecool/citystatistics/instance_creator/CitySmallCardCreator.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/instance_creator/CitySmallCardCreator.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CitySmallCardCreator {
-    CitySmallCard createCitySmallCard(){
+    public CitySmallCard createCitySmallCard(){
         return new CitySmallCard();
     }
 }

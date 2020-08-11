19048fbc736212b040fccbb1e0ae58f5d3228246
Jorakasi <gabor.olah0323@gmail.com>
2020-08-11 13:37:48 +0200

    FavoriteCityRepository added

--- citystatistics/src/main/java/com/codecool/citystatistics/repository/FavouriteCityRepository.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/repository/FavouriteCityRepository.java
@@ -2,7 +2,9 @@ package com.codecool.citystatistics.repository;
 
 import com.codecool.citystatistics.entity.FavouriteCity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface FavouriteCityRepository extends JpaRepository<FavouriteCity, Long> {
+    int deleteFavouriteCityBySlug(@Param("slug") String slug);
 
 }

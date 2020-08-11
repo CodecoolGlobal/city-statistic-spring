58f434d3673096129fda41a12078148bb8f1e32b
aron <pachl.aron@gmail.com>
2020-08-11 14:55:43 +0200

    unused import deleted

--- citystatistics/src/main/java/com/codecool/citystatistics/repository/CommentRepository.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package com.codecool.citystatistics.repository;
 
 import com.codecool.citystatistics.entity.Comment;
-import com.codecool.citystatistics.entity.FavouriteCity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;

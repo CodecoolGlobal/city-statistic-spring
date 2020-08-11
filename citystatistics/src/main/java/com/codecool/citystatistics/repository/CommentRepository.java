568ba9c934877ec30abe3eb0225a987a247e523b
Jorakasi <gabor.olah0323@gmail.com>
2020-08-11 15:17:52 +0200

    Comment repository query created

--- citystatistics/src/main/java/com/codecool/citystatistics/repository/CommentRepository.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/repository/CommentRepository.java
@@ -3,9 +3,12 @@ package com.codecool.citystatistics.repository;
 import com.codecool.citystatistics.entity.Comment;
 import com.codecool.citystatistics.entity.FavouriteCity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllBySlug(String slug);
+
+    @Query(" SELECT Comment.comment from Comment WHERE slug=:slug")
+    List<Comment> getAllCommentsBySlug(String slug);
 }

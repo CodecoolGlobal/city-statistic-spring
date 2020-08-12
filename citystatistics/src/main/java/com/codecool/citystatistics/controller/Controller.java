a998f7d5adcc76432bc7b5c97200c2db7c924fb3
TiborGulyas <gulyastibor@gmail.com>
2020-08-12 14:57:19 +0200

    get all favourite slugs - completed

--- citystatistics/src/main/java/com/codecool/citystatistics/controller/Controller.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/controller/Controller.java
@@ -35,24 +35,22 @@ public class Controller {
     @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
     public ArrayList<CitySmallCard> continentCities(@PathVariable String continent) throws IOException, JSONException {
         ArrayList<CitySmallCard> citySmallCards = new ArrayList<>();
-        String searchContinentURL = "https://api.teleport.org/api/continents/geonames%3A"+continent+"/urban_areas/";
+        String searchContinentURL = "https://api.teleport.org/api/continents/geonames%3A" + continent + "/urban_areas/";
         JSONObject resultSlugs = apiCall.getResult(searchContinentURL);
         JSONArray slugs = resultSlugs.getJSONObject("_links").getJSONArray("ua:items");
 
         List<String> favouriteCitySlugs = favouriteCityRepository.getAllFavouriteSlug();
 
-        for (int i = 0; i < slugs.length(); i++){
+        for (int i = 0; i < slugs.length(); i++) {
 
             JSONObject citySlug = slugs.getJSONObject(i);
-            String name =  citySlug.getString("name");
-            String slug = citySlug.getString("href").substring(citySlug.getString("href").indexOf("slug:")+5, citySlug.getString("href").length()-1);
-            
-            JSONObject resultImage = apiCall.getResult(citySlug.getString("href").replaceAll("\\\\", "")+"images");
+            String name = citySlug.getString("name");
+            String slug = citySlug.getString("href").substring(citySlug.getString("href").indexOf("slug:") + 5, citySlug.getString("href").length() - 1);
+
+            JSONObject resultImage = apiCall.getResult(citySlug.getString("href").replaceAll("\\\\", "") + "images");
             String imageURL = resultImage.getJSONArray("photos").getJSONObject(0).getJSONObject("image").getString("web");
 
             boolean isFavourite = favouriteCitySlugs.contains(slug);
-
-
 
 
             CitySmallCard cityCard = CitySmallCard
@@ -70,21 +68,20 @@ public class Controller {
     }
 
 
-
     @GetMapping("/cityalldata/{cityslug}")
     @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
     public CityAllData result(@PathVariable String cityslug) throws IOException, JSONException {
-        String URL = "https://api.teleport.org/api/urban_areas/slug:"+cityslug;
+        String URL = "https://api.teleport.org/api/urban_areas/slug:" + cityslug;
         ArrayList<Score> scoreArrayList = new ArrayList<>();
         ArrayList<Salary> salaryArrayList = new ArrayList<>();
         ArrayList<String> comments = commentRepository.getAllCommentsBySlug(cityslug);
 
 
-        JSONObject resultName = apiCall.getResult(URL  + "/");
+        JSONObject resultName = apiCall.getResult(URL + "/");
         String name = resultName.getString("name");
 
 
-        JSONObject resultScores = apiCall.getResult(URL  + "/scores/");
+        JSONObject resultScores = apiCall.getResult(URL + "/scores/");
         JSONArray categories = (JSONArray) resultScores.get("categories");
 
         for (int i = 0; i < categories.length(); i++) {
@@ -98,11 +95,11 @@ public class Controller {
         }
 
 
-        JSONObject resultSalaries = apiCall.getResult(URL  + "/salaries/");
+        JSONObject resultSalaries = apiCall.getResult(URL + "/salaries/");
         JSONArray salaries = (JSONArray) resultSalaries.get("salaries");
         for (int i = 0; i < salaries.length(); i++) {
-            
-            String title =salaries.getJSONObject(i).getJSONObject("job").getString("title");
+
+            String title = salaries.getJSONObject(i).getJSONObject("job").getString("title");
             int percentile_25 = salaries.getJSONObject(i).getJSONObject("salary_percentiles").getInt("percentile_25");
             int percentile_50 = salaries.getJSONObject(i).getJSONObject("salary_percentiles").getInt("percentile_50");
             int percentile_75 = salaries.getJSONObject(i).getJSONObject("salary_percentiles").getInt("percentile_75");
@@ -135,10 +132,10 @@ public class Controller {
     public void addFavouriteCity(@PathVariable String citySlug) {
 
         try {
-            if(PreDefinedSlugSet.preDefinedSlugSet.contains(citySlug)){
+            if (PreDefinedSlugSet.preDefinedSlugSet.contains(citySlug)) {
                 favouriteCityRepository.save(FavouriteCity.builder().slug(citySlug).build());
             }
-        } catch ( DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             System.out.println("Error: " + e);
         }
     }
@@ -148,10 +145,10 @@ public class Controller {
     public void deleteFavouriteCity(@PathVariable String citySlug) {
 
         try {
-            if(PreDefinedSlugSet.preDefinedSlugSet.contains(citySlug)){
+            if (PreDefinedSlugSet.preDefinedSlugSet.contains(citySlug)) {
                 favouriteCityRepository.deleteFavouriteCityBySlug(citySlug);
             }
-        } catch ( DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             System.out.println("Error: " + e);
         }
     }
@@ -163,17 +160,43 @@ public class Controller {
         System.out.println(receivedComment.getString("comment"));
         System.out.println("query comment: " + comment);
         try {
-            if(PreDefinedSlugSet.preDefinedSlugSet.contains(citySlug)){
+            if (PreDefinedSlugSet.preDefinedSlugSet.contains(citySlug)) {
                 commentRepository.save(Comment.builder().slug(citySlug).comment(receivedComment.getString("comment")).build());
             }
-        } catch ( DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             System.out.println("Error: " + e);
         }
     }
 
     @GetMapping("/get-all-favourite-cities")
     @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
-    public ArrayList<String> getAllFavouriteCitySlugs(){
-        return favouriteCityRepository.getAllFavouriteSlug();
+    public ArrayList<CitySmallCard> getAllFavouriteCitySlugs() throws IOException, JSONException {
+
+        ArrayList<String> allFavouriteSlug = favouriteCityRepository.getAllFavouriteSlug();
+        ArrayList<CitySmallCard> citySmallCards = new ArrayList<>();
+        for (String favouriteSlug : allFavouriteSlug){
+            String URL = "https://api.teleport.org/api/urban_areas/slug:"+favouriteSlug;
+            JSONObject resultCityData = apiCall.getResult(URL+"/");
+            String name = resultCityData.getString("name");
+
+            JSONObject resultImage = apiCall.getResult(URL + "/images/");
+            String imageURL = resultImage.getJSONArray("photos").getJSONObject(0).getJSONObject("image").getString("web");
+
+            CitySmallCard cityCard = CitySmallCard
+                    .builder()
+                    .cityName(name)
+                    .citySlug(favouriteSlug)
+                    .cityImage(imageURL)
+                    .isFavourite(true)
+                    .build();
+
+
+            citySmallCards.add(cityCard);
+        }
+        return citySmallCards;
     }
+
+
+
+
 }

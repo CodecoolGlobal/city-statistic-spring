29feabeaad7e00617d44c5a8dd19e8910b3a5eb8
aron <pachl.aron@gmail.com>
2020-08-25 11:13:07 +0200

    deleted def user

--- citystatistics/src/main/java/com/codecool/citystatistics/CityStatisticsApplication.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/CityStatisticsApplication.java
@@ -1,18 +1,7 @@
 package com.codecool.citystatistics;
 
-import com.codecool.citystatistics.entity.AppUser;
-import com.codecool.citystatistics.entity.FavouriteCity;
-import com.codecool.citystatistics.repository.AppUserRepository;
-import com.codecool.citystatistics.repository.FavouriteCityRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import java.time.LocalDate;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class CityStatisticsApplication {

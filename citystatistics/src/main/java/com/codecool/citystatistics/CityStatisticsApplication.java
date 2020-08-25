eb2e25aa6870663890058cd79c92039e48e8ac8e
aron <pachl.aron@gmail.com>
2020-08-25 10:05:10 +0200

    default user added

--- citystatistics/src/main/java/com/codecool/citystatistics/CityStatisticsApplication.java
+++ citystatistics/src/main/java/com/codecool/citystatistics/CityStatisticsApplication.java
@@ -1,20 +1,40 @@
 package com.codecool.citystatistics;
 
+import com.codecool.citystatistics.entity.AppUser;
 import com.codecool.citystatistics.entity.FavouriteCity;
+import com.codecool.citystatistics.repository.AppUserRepository;
 import com.codecool.citystatistics.repository.FavouriteCityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CityStatisticsApplication {
 
-
+    @Autowired
+    AppUserRepository appUserRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CityStatisticsApplication.class, args);
+    }
+
+    @Bean
+    @Profile("production")
+    public CommandLineRunner init(){
+        return args -> {
+            AppUser user = AppUser.builder()
+                    .username("admin").birthDate(LocalDate.of(1999,8,8))
+                    .password("password").email("admin@gmail.com").roles(Arrays.asList("ROLE_ADMIN", "ROLE_USER"))
+                    .build();
+
+            appUserRepository.save(user);
+        };
     }
 
 }
